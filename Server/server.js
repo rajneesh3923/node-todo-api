d878e40ce6222a0e165245dba8dfd84487813d42
rajneesh3923 <rajneeshchaurasia6@gmail.com>
2018-11-29 14:13:26 +0530

    Added JWT Authentication And password Hashing using Bcrypt

--- Server/server.js
+++ Server/server.js
@@ -6,7 +6,7 @@ var {ObjectID} = require('mongodb');
 var {mongoose} = require('./db/mongoose');
 var { Todo } = require('./models/todo');
 var { User } = require('./models/user');
-
+var {authenticate} = require('./middleware/authenticate');
 
 var app = express();
 const port = process.env.PORT || 3000;
@@ -89,6 +89,13 @@ app.post('/users', (req,res) => {
     });
 });
 
+
+
+app.get('/users/me',authenticate,(req,res) => {
+
+            res.send(req.user);
+
+});
 
 
 
